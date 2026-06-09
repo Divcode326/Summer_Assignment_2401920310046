d4ab6bbe53353634909dd27eb8636aef6ccc22d0
Divcode326 <24121cn141@glbitm.ac.in>
2026-06-09 22:47:21 +0530

    Create Permutation_in_String.cpp

---
+++ Week_2/Day_2/Permutation_in_String.cpp
@@ -0,0 +1,31 @@
+class Solution:
+    def checkInclusion(self, s1, s2):
+        if len(s1) > len(s2):
+            return False
+
+        count1 = {}
+        count2 = {}
+
+        for ch in s1:
+            count1[ch] = count1.get(ch, 0) + 1
+
+        for i in range(len(s1)):
+            count2[s2[i]] = count2.get(s2[i], 0) + 1
+
+        if count1 == count2:
+            return True
+
+        left = 0
+        for right in range(len(s1), len(s2)):
+            count2[s2[right]] = count2.get(s2[right], 0) + 1
+
+            count2[s2[left]] -= 1
+            if count2[s2[left]] == 0:
+                del count2[s2[left]]
+
+            left += 1
+
+            if count1 == count2:
+                return True
+
+        return False
