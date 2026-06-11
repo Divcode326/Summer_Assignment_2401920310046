546ec920cfe6ebf27ce2b5df0409e2169160e4dc
Divcode326 <24121cn141@glbitm.ac.in>
2026-06-11 14:58:04 +0530

    Create Generate_Parentheses.cpp

---
+++ Week_2/Day_4/Generate_Parentheses.cpp
@@ -0,0 +1,17 @@
+class Solution(object):
+    def generateParenthesis(self, n):
+        result = []
+
+        def backtrack(curr, openCount, closeCount):
+            if len(curr) == 2 * n:
+                result.append(curr)
+                return
+
+            if openCount < n:
+                backtrack(curr + "(", openCount + 1, closeCount)
+
+            if closeCount < openCount:
+                backtrack(curr + ")", openCount, closeCount + 1)
+
+        backtrack("", 0, 0)
+        return result
